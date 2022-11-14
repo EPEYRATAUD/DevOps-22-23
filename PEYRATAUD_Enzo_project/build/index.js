e020e624b4b5e5b5a8bbee890b8522f5af2369a3
enzowar <lucas.peyrataud@gmail.com>
2022-11-14 17:07:38 +0100

    DevOps tp 1

---
+++ PEYRATAUD_Enzo_project/build/index.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)(); // Permet l'utilisation de express pour le serveur web.
+const PING_LISTEN_PORT = 8080; // Définit le port sur lequel se lance le serveur web
+// ACCUEIL //
+app.get("/", (req, res) => {
+    res.send("This is a test web page!");
+});
+//REQUETE HEADERS//
+app.get("/PING", (req, res) => {
+    res.send(req.headers);
+});
+// ERREUR 404 //
+app.all("*", (req, res) => {
+    res.status(404).send();
+});
+app.listen(PING_LISTEN_PORT, () => {
+    console.log("The application is listening on port " + PING_LISTEN_PORT + " !");
+});
